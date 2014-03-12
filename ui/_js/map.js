711b75c3713c1f804e18ecc9e6ea3dd86583b401
William Stam <awstam@gmail.com>
2014-03-12 13:10:04 +0200

    latest

--- ui/_js/map.js
+++ ui/_js/map.js
@@ -387,10 +387,9 @@ function initialize() {
 		for (var i = 0, place; place = places[i]; i++) {
 			var image = {
 				url       : place.icon,
-				size      : new google.maps.Size(71, 71),
+				
 				origin    : new google.maps.Point(0, 0),
-				anchor    : new google.maps.Point(17, 34),
-				scaledSize: new google.maps.Size(25, 25)
+				scaledSize: new google.maps.Size(50, 50)
 			};
 
 			// Create a marker for each place.
